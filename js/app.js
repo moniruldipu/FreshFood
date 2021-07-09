d291b0a71cfb7223da02fcf730caecb33a293a9e
moniruldipu <info.moniruldipu@gmail.com>
2021-07-10 00:08:16 +0600

    responsive done

--- js/app.js
+++ js/app.js
@@ -5,5 +5,26 @@ $(document).ready(function(){
         slidesToScroll:1,
         prevArrow:".prev-btn",
         nextArrow:".next-btn",
+
+        responsive:[
+            {
+                breakpoint:992,
+                settings:{
+                    slidesToShow:2,
+                }
+            },
+            {
+                breakpoint:768,
+                settings:{
+                    slidesToShow:1,
+                }
+            }
+        ]
+
     });
+
+$('.nav-trigger').click(function(){
+    $('.site-content-wrapper').toggleClass('scaled');
+})
+
 });
